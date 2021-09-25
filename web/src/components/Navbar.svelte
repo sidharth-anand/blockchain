132a782fb2dfa8aafd88914ad91aca553f01fd4e
Sreekar Venkata Nutulapati <sreekarnv1@gmail.com>
2021-09-25 19:05:31 +0530

    adding final changes

--- web/src/components/Navbar.svelte
+++ web/src/components/Navbar.svelte
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg d-flex justify-content-between">
 	<div class="header-title flex-fill">
-		<h5>Blockchain</h5>
+		<h5>Dexter Coffee Shop</h5>
 	</div>
 	<a
 		href="https://github.com/sidharth-anand/blockchain.git"
